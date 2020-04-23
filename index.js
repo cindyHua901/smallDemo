817a8b6dab68b9b0efbd00032d0bf08a3d7ebb3e
Cindy <guohua@wctsoft.com>
2020-04-23 11:39:37 +0800

    feat 插件机制，注入依赖

--- index.js
+++ index.js
@@ -5,55 +5,13 @@ window.onload=function(){
   // slideTo()
   // slidePreivous()
   class Slider{
-    constructor(id, cycle=3000){
+    constructor(id, cycle = 3000){
       this.container = document.getElementById(id);
       this.items = this.container.querySelectorAll('.slider-list__item, .slider-list__item--selected');
       this.cycle = cycle;
-
-      const controller = this.container.querySelector('.slide-list__control');
-      if(controller){
-        const buttons = controller.querySelectorAll(".slide-list__control-buttons, .slide-list__control-buttons--selected");
-        controller.addEventListener("mouseover",evt=>{
-          // 比较是哪一个btn有mouseover的动作 获取btn的idx
-          const idx = Array.from(buttons).indexOf(evt.target);
-          if(idx >= 0) {
-            this.slideTo(idx);
-            this.stop();
-          }
-        });
-
-        controller.addEventListener("mouseout", evt=>{
-          this.start();
-        });
-              
-      // 自定义slide事件，保持btn与图片的index同步
-      this.container.addEventListener('slide', evt => {
-        const idx = evt.detail.index
-        const selected = controller.querySelector('.slide-list__control-buttons--selected');
-        if(selected) selected.className = 'slide-list__control-buttons';
-        buttons[idx].className = 'slide-list__control-buttons--selected';
-      })
-      }
-
-      const previous  = this.container.querySelector(".slider-list__previous");
-      if(previous){
-        previous.addEventListener('click',evt=>{
-          this.stop();
-          this.slidePrevious();
-          this.start();
-          evt.preventDefault()
-        })
-      }
-      const next  = this.container.querySelector(".slider-list__next");
-      if(next){
-        next.addEventListener('click',evt=>{
-          this.stop();
-          this.slideNext();
-          this.start();
-          evt.preventDefault()
-        })
-      }
-      
+    }
+    registerPlugins(...plugins){
+      plugins.forEach(plugin => plugin(this));
     }
     getSelectedItem(){
       const selected = this.container.querySelector('.slider-list__item--selected');
@@ -71,30 +29,84 @@ window.onload=function(){
       if(item){
         item.className = 'slider-list__item--selected';
       }
+  
       const detail = {index: idx}
       const event = new CustomEvent('slide', {bubbles:true, detail})
       this.container.dispatchEvent(event)
     }
     slideNext(){
-      let currentIdx = this.getSelectedItemIndex();
-      let nextIdx = (currentIdx + 1) % this.items.length;
+      const currentIdx = this.getSelectedItemIndex();
+      const nextIdx = (currentIdx + 1) % this.items.length;
       this.slideTo(nextIdx);
     }
     slidePrevious(){
-      let currentIdx = this.getSelectedItemIndex();
-      let previousIdx = (this.items.length + currentIdx - 1) % this.items.length;
+      const currentIdx = this.getSelectedItemIndex();
+      const previousIdx = (this.items.length + currentIdx - 1) % this.items.length;
       this.slideTo(previousIdx);  
     }
-
+    addEventListener(type, handler){
+      this.container.addEventListener(type, handler)
+    }
     start(){
       this.stop();
       this._timer = setInterval(()=>this.slideNext(), this.cycle);
     }
-
-    stop() {
+    stop(){
       clearInterval(this._timer);
     }
   }
+  
+  function pluginController(slider){
+    const controller = slider.container.querySelector('.slide-list__control');
+    if(controller){
+      const buttons = controller.querySelectorAll('.slide-list__control-buttons, .slide-list__control-buttons--selected');
+      controller.addEventListener('mouseover', evt=>{
+        const idx = Array.from(buttons).indexOf(evt.target);
+        if(idx >= 0){
+          slider.slideTo(idx);
+          slider.stop();
+        }
+      });
+  
+      controller.addEventListener('mouseout', evt=>{
+        slider.start();
+      });
+  
+      slider.addEventListener('slide', evt => {
+        const idx = evt.detail.index
+        const selected = controller.querySelector('.slide-list__control-buttons--selected');
+        if(selected) selected.className = 'slide-list__control-buttons';
+        buttons[idx].className = 'slide-list__control-buttons--selected';
+      });
+    }  
+  }
+  
+  function pluginPrevious(slider){
+    const previous = slider.container.querySelector('.slide-list__previous');
+    if(previous){
+      previous.addEventListener('click', evt => {
+        slider.stop();
+        slider.slidePrevious();
+        slider.start();
+        evt.preventDefault();
+      });
+    }  
+  }
+  
+  function pluginNext(slider){
+    const next = slider.container.querySelector('.slide-list__next');
+    if(next){
+      next.addEventListener('click', evt => {
+        slider.stop();
+        slider.slideNext();
+        slider.start();
+        evt.preventDefault();
+      });
+    }  
+  }
+  
+  
   const slider = new Slider('my-slider');
-  slider.start()
+  slider.registerPlugins(pluginController, pluginPrevious, pluginNext);
+  slider.start();
 }
